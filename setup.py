2a29ee3718381c3567c7c3a5fa1dca8693fb65e0
David <davidyen1124@gmail.com>
2014-11-26 12:56:18 +0800

    fix license not found

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup
 
-license = open('LICENSE.txt').read()
-
 setup(
     name='facebot',
-    version='0.0.2',
+    version='0.0.3',
     author='davidyen1124',
     author_email='davidyen1124@gmail.com',
     packages=['facebot'],
     url='https://github.com/davidyen1124/Facebot',
     description='Unofficial facebook api',
-    long_description=open('README.md').read(),
-    license=license,
+    license='MIT',
     install_requires=[
         'lxml==3.3.5',
         'requests==2.3.0'
