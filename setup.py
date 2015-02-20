29d087a82313e5a152c0e12ac41b30869059e656
David <davidyen1124@gmail.com>
2015-02-20 23:29:19 +0800

    bump to version 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='facebot',
-    version='0.0.7',
+    version='0.0.8',
     author='davidyen1124',
     author_email='davidyen1124@gmail.com',
     packages=['facebot'],
