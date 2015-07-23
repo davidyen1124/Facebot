595cbb3aedf227f5ba0d48d64f335f19a9b9e108
spartak1547 <sinisa.colic@gmail.com>
2015-07-24 01:14:37 +0200

    Adjusted to facebook change
    Facebook now uses javascript to add 3 cookies and forbids login when browser is missing them.

--- facebot/__init__.py
+++ facebot/__init__.py
@@ -13,7 +13,7 @@ from facebot.sticker import get_stickers
 
 logging.basicConfig()
 log = logging.getLogger('facebook')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARN)
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'
 
